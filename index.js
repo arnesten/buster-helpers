fe513a38e12799de91e43c6c54fba3d8cbe46a0d
Calle Arnesten <calle.arnesten@codekick.com>
2012-11-14 11:08:57 +0100

    Renamed from "buster-autotest". Added teamcity helper.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var autoTester = require('./lib/autoTester.js');
 var testRunner = require('./lib/testRunner.js');
 
-module.exports.start = function (sourceDir, testDir) {
+module.exports.startAutoTest = function (sourceDir, testDir) {
     autoTester.start(sourceDir, testDir);
 };
 
 module.exports.runOnce = function (testDir) {
-    testRunner.run(testDir);
+    testRunner.runDots(testDir);
+};
+
+module.exports.runOnceTeamcity = function (testDir) {
+    testRunner.runTeamcity(testDir);
 };
