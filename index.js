905652f3feb60e5c1ef96f3269501bd230088954
Calle Arnesten <calle.arnesten@codekick.com>
2012-11-14 10:44:43 +0100

    Exposed runOnce() method

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var autoTester = require('./lib/autoTester.js');
+var testRunner = require('./lib/testRunner.js');
 
 module.exports.start = function (sourceDir, testDir) {
     autoTester.start(sourceDir, testDir);
+};
+
+module.exports.runOnce = function (testDir) {
+    testRunner.run(testDir);
 };
